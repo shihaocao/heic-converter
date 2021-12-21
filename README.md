ea414b3a6f7b8c23a3afab2e9583ad9a87005f8f
Shihao Cao <shehowcow@gmail.com>
2021-12-20 19:55:02 -0500

    update readme and add script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # heic-converter
 A python3 script using heif-convert to batch convert heic files
+
+# Installation
+Install `heif-convert` using: https://ubuntuhandbook.org/index.php/2021/06/open-heic-convert-jpg-png-ubuntu-20-04/
+
+Copy the included python3 script to the target directory that you are doing the conversion.
+# Usage
+In the target directory of conversion, run:
+
+```
+python3 heic-conv.py
+
+```
+
+This will convert all `.heic` files to `.jpg` at 99% quality. Edit the script to dial your desired quality.
---
+++ heic-conv.py
@@ -0,0 +1,22 @@
+import glob, os, subprocess
+os.chdir("/home/shihao/Downloads/")
+
+cmd = "heif-convert"
+QUALITY = "-q"
+QUALITY_ARG = "99"
+
+for file in glob.glob("*.heic"):
+    base_file = file[:-5]
+    new_file = base_file + ".jpg"
+    args = [cmd, QUALITY, QUALITY_ARG, file, new_file]
+    popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+    popen.wait()
+    output = popen.stdout.read()
+    string_output = output.decode("utf-8")
+
+    # delete the aux file
+    aux_file_name = base_file + "-urn:com:apple:photo:2020:aux:hdrgainmap" + ".jpg"
+    os.remove(aux_file_name)
+
+    print(string_output)
+
